fa75e2bcb20810c786433a4b153c6f725f4489d7
Clément Vuchener <clement.vuchener@gmail.com>
2015-09-27 22:05:49 +0200

    fix wrong index

--- main.cpp
+++ main.cpp
@@ -114,7 +114,7 @@ int main (int argc, char *argv[])
 
 	ret = libusb_control_transfer (device,
 				       LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE,
-				       REQUEST_SET_COLOR, color[1] << 8 | color[2], target << 8 | color[3],
+				       REQUEST_SET_COLOR, color[1] << 8 | color[2], target << 8 | color[0],
 				       nullptr, 0, 0);
 	if (ret < 0) {
 		fprintf (stderr, "Failed to get hardware mode: %s (%s)\n",
